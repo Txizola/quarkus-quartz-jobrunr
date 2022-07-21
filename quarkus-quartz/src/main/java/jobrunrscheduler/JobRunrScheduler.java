df849b82801223a86510a47d9135178b14b8c868
Diogo Teixeira <diogo.jesus.tx@live.com.pt>
2022-07-21 17:59:23 +0100

    JobRunr funcionável

--- quarkus-quartz/src/main/java/jobrunrscheduler/JobRunrScheduler.java
+++ quarkus-quartz/src/main/java/jobrunrscheduler/JobRunrScheduler.java
@@ -5,14 +5,14 @@ import dataModels.HTTPTask;
 import dataModels.Schedule;
 import org.jobrunr.jobs.JobId;
 import org.jobrunr.scheduling.JobScheduler;
-import org.jobrunr.storage.StorageProvider;
-import org.jobrunr.storage.nosql.mongo.MongoDBStorageProvider;
 import org.quartz.SchedulerException;
+import java.time.Duration;
+import java.time.Instant;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-
+@ApplicationScoped
 public class JobRunrScheduler implements Schedule {
 
     @Inject
@@ -21,24 +21,10 @@ public class JobRunrScheduler implements Schedule {
     @Inject
     JobScheduler jobScheduler;
 
-    public JobRunrScheduler() throws SchedulerException {}
-
-    /*instance
-    private static JobRunrScheduler jobRunrScheduler;
-    static {
-        try {
-            jobRunrScheduler = new JobRunrScheduler();
-        } catch (SchedulerException e) {
-            e.printStackTrace();
-        }
-    }
-    public static JobRunrScheduler getInstance() {
-        return jobRunrScheduler;
-    }*/
-
+    public JobRunrScheduler(){}
 
     public void schedule(HTTPTask httpTask, Delay delay) throws InterruptedException {
-        final JobId enqueuedJobId = jobScheduler.enqueue(() -> jobClassJobRunr.doJob(httpTask));
+        final JobId enqueuedJobId = jobScheduler.schedule(Instant.now().plusSeconds(delay.getDelay()), () -> jobClassJobRunr.doJob(httpTask));
         System.out.println("job enqueued = " + enqueuedJobId);
     }
 }
